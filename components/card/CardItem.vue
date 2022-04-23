f2464a32d50b94f59f431f63c42f7e13cd1c9136
Fandi Maulana <fandimaulana936@gmail.com>
2022-04-23 12:18:50 +0700

    feat: routing nuxtjs

---
+++ components/card/CardItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card mb-3 mx-5">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img :src="article.img" class="img-fluid rounded m-2" alt="..." />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <nuxt-link class="title-task mb-1" :to="'detail/' + article.id">
+            <h5 class="card-title">{{ article.title }}</h5>
+          </nuxt-link>
+          <p class="card-text">
+            {{ article.content }}
+          </p>
+          <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    article: {
+      type: Object,
+      default: "Article",
+    },
+  },
+};
+</script>
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+router: {
+  routes: [
+    {
+      name: "index",
+      path: "/",
+      component: "pages/index.vue",
+    },
+    {
+      name: "user",
+      path: "/user",
+      component: "pages/user/index.vue",
+    },
+    {
+      name: "user-one",
+      path: "/user/one",
+      component: "pages/user/one.vue",
+    },
+  ];
+}
